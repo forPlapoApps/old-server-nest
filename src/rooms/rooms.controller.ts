f7aec8d3062554dbdd8ca19776a7d2fd8ef25e61
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-05 02:20:13 +0900

    feat: prismaを用いたroomのcrud

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,16 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { RoomsService } from './rooms.service';
-import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
-
-  @Post()
-  create(@Body() createRoomDto: CreateRoomDto) {
-    return this.roomsService.create(createRoomDto);
-  }
 
   @Get()
   findAll() {
@@ -19,16 +13,21 @@ export class RoomsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.roomsService.findOne(+id);
+    return this.roomsService.findOne(id);
   }
 
-  @Patch(':id')
+  @Post()
+  create() {
+    return this.roomsService.create();
+  }
+
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
-    return this.roomsService.update(+id, updateRoomDto);
+    return this.roomsService.update(id, updateRoomDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.roomsService.remove(+id);
+    return this.roomsService.remove(id);
   }
 }
--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRoomDto } from './dto/create-room.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UpdateRoomDto } from './dto/update-room.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class RoomsService {
-  create(createRoomDto: CreateRoomDto) {
-    return 'This action adds a new room';
+  private readonly prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
   }
 
-  findAll() {
-    return `This action returns all rooms`;
+  async findAll() {
+    const rooms = await this.prisma.room.findMany();
+    return rooms
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} room`;
+  async findOne(id: string) {
+    const room = await this.prisma.room.findFirst({ where: { id }})
+    if (!room) {
+      throw new NotFoundException()
+    }
+    return room
+  }
+  
+  async create() {
+    const date = new Date
+    const room = await this.prisma.room.create({
+      data: {
+        name: date.toString()
+      }
+    })
+    return room;
   }
 
-  update(id: number, updateRoomDto: UpdateRoomDto) {
-    return `This action updates a #${id} room`;
+  async update(id: string, updateRoomDto: UpdateRoomDto) {
+    const room = await this.prisma.room.update({
+      where: { id },
+      data: updateRoomDto
+    })
+    return room;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} room`;
+  async remove(id: string) {
+    const room = await this.prisma.room.delete({
+      where: { id }
+    });
+    return room;
   }
 }
