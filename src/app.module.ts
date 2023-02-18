95677f0fba0b4d7645da8a2a4247c098371885a1
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-19 04:23:21 +0900

    fix: ディレクトリ名の修正 user→users #14

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
-import { UserModule } from './user/user.module';
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [RoomsModule, UserModule],
