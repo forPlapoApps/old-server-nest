20027dfe9ad2e0eca78979dada7e7cb204669fe8
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-22 06:55:52 +0900

    feat: vote#create #27

--- src/votes/votes.service.ts
+++ src/votes/votes.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Vote } from '@prisma/client';
+import { CreateVoteDto } from './dto/create-vote.dto';
 
 @Injectable()
-export class VotesService {}
+export class VotesService {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(createVoteDto: CreateVoteDto): Promise<Vote> {
+    const vote = await this.prisma.vote.create({
+      data: {
+        plapo: {
+          connect: {
+            id: createVoteDto.plapoId,
+          },
+        },
+        user: {
+          connect: {
+            id: createVoteDto.userId,
+          },
+        },
+      },
+    });
+
+    return vote;
+  }
+}
