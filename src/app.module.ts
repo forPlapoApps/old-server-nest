5d2a92c76147ffef0bce48c4b202aa3c1116a185
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-22 06:47:05 +0900

    feat: generate vote resource #27

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
 import { UserModule } from './users/users.module';
 import { PlapoModule } from './plapo/plapo.module';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [RoomsModule, UserModule, PlapoModule],
+  imports: [RoomsModule, UserModule, PlapoModule, VotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
