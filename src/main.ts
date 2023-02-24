79c2e83d76fa740484f10c11e8758ba78d8393aa
hulk-higakijin <hm385.chejptks@gmail.com>
2023-02-24 13:21:06 +0900

    feat: to deploy GCP, setup port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8000);
+  // await app.listen(8000);
+
+  const port = Number(process.env.PORT) || 8000;
+  await app.listen(port, '0.0.0.0')
 }
 bootstrap();
